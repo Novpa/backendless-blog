326ef296850ffe6e2657efd3ddae0ecc56854445
Novpa <aa.novparadiputra@binus.ac.id>
2026-02-26 23:31:17 +0700

    ref/fe form validation

--- backendless-blog/src/pages/Landing/Landing.tsx
+++ backendless-blog/src/pages/Landing/Landing.tsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
-import { FaArrowRightLong } from "react-icons/fa6";
 import ButtonCta from "../../ui/ButtonCta";
 
 function Landing() {
   return (
     <main className="px-30 pt-30">
-      <div className="">
-        <div className="text-6xl font-semibold py-5">
-          <h1 className="py-2">Welcome do Loggy</h1>
-          <h2 className="py-2">Pour your Logs</h2>
+      <div>
+        <div className="text-6xl  font-semibold py-5">
+          <h1 className="py-2 flex bg-linear-to-r from-white to-black bg-clip-text  text-transparent">
+            Welcome do Loggy
+          </h1>
+          <h2 className="py-2 bg-linear-to-r flex from-white to-black bg-clip-text  text-transparent">
+            Pour your Logs
+          </h2>
         </div>
 
         <div className="w-[50%] text-stone-400 pb-8">
--- backendless-blog/src/pages/adminDashboard/CreateBlogPage.tsx
+++ backendless-blog/src/pages/adminDashboard/CreateBlogPage.tsx
@@ -2,8 +2,19 @@ import { useFormik } from "formik";
 import createBlogPageSchema from "../../features/adminDashboard/schemas/createBlogPageSchema";
 import type { CreateBlogType } from "../../interfaces/createBlogPage";
 import Backendless from "../../backendless/backendless";
+import { useState } from "react";
+import CofirmationModal from "../../ui/CofirmationModal";
+import Button from "../../ui/Button";
+import FormErrorMessage from "../../ui/FormErrorMessage";
 
 function CreateBlogPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  //handle show modal
+  const handleShowModal = () => {
+    setShowModal((show) => !show);
+  };
+
   //handle submit
   const handleSubmitBlog = async (values: CreateBlogType) => {
     try {
@@ -31,68 +42,108 @@ function CreateBlogPage() {
     },
   });
 
+  const isWrongInput =
+    formik.errors.title !== undefined ||
+    formik.errors.author !== undefined ||
+    formik.errors.content !== undefined;
+
+  const isNotEmpty =
+    formik.values.title !== "" &&
+    formik.values.author !== "" &&
+    formik.values.content !== "";
+
+  const isError = isWrongInput || !isNotEmpty;
+
+  console.log("formik.errors.title", formik.errors.title);
+  console.log("isWrongInput", isWrongInput);
+  console.log("isNotEmpty", isNotEmpty);
+  console.log("isError", isError);
+
   return (
-    <div>
-      <div className="flex justify-center pt-10">
-        <h1>Create blog page</h1>
+    <main className="pt-12 h-dvh">
+      <div>
+        <div className="flex justify-center pt-12 text-3xl pb-10 ">
+          <h1 className="bg-linear-to-r from-white to-stone-500 bg-clip-text  text-transparent">
+            Create Log Form
+          </h1>
+        </div>
+
+        <section className="flex justify-center">
+          <div>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="flex flex-col pb-5">
+                <label className="text-stone-400 text-lg font-light">
+                  Blog title
+                </label>
+                <input
+                  id="title"
+                  name="title"
+                  onChange={formik.handleChange}
+                  value={formik.values?.title}
+                  type="text"
+                  placeholder="Input your new Log title"
+                  className="border w-100 h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                />
+                {formik?.errors && (
+                  <FormErrorMessage>{formik?.errors?.title}</FormErrorMessage>
+                )}
+              </div>
+
+              <div className="flex flex-col pb-5">
+                <label className="text-stone-400 text-lg font-light">
+                  Author
+                </label>
+                <input
+                  id="author"
+                  name="author"
+                  onChange={formik.handleChange}
+                  value={formik.values?.author}
+                  type="text"
+                  placeholder="Input your name as 'Author'"
+                  className="border w-100 h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                />
+                {formik?.errors && (
+                  <FormErrorMessage>{formik?.errors?.author}</FormErrorMessage>
+                )}
+              </div>
+
+              <div className="flex flex-col pb-5">
+                <label className="text-stone-400 text-lg font-light">
+                  Content
+                </label>
+                <textarea
+                  id="content"
+                  name="content"
+                  onChange={formik.handleChange}
+                  value={formik.values?.content}
+                  placeholder="Drop your ideas"
+                  className="border w-100 h-30 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                  maxLength={5000}
+                />
+                {formik?.errors && (
+                  <FormErrorMessage>{formik?.errors?.content}</FormErrorMessage>
+                )}
+                {showModal && (
+                  <CofirmationModal
+                    type="submit"
+                    onCloseModal={handleShowModal}>
+                    Are you sure want to submit this post? You can edit it later
+                  </CofirmationModal>
+                )}
+              </div>
+            </form>
+            {showModal || (
+              <Button
+                disabled={isError}
+                type="button"
+                onClick={handleShowModal}>
+                Submit
+              </Button>
+            )}
+          </div>
+        </section>
       </div>
-
-      <section className="flex justify-center">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="flex flex-col pb-5">
-            <label>Blog title</label>
-            <input
-              id="title"
-              name="title"
-              onChange={formik.handleChange}
-              value={formik.values?.title}
-              type="text"
-              placeholder="Input your new Log title"
-              className="border border-indigo"
-            />
-            {formik?.errors && (
-              <p className="text-xs text-red-700">{formik?.errors?.title}</p>
-            )}
-          </div>
-
-          <div className="flex flex-col pb-5">
-            <label>Author</label>
-            <input
-              id="author"
-              name="author"
-              onChange={formik.handleChange}
-              value={formik.values?.author}
-              type="text"
-              placeholder="Input your name as 'Author'"
-              className="border border-indigo"
-            />
-            {formik?.errors && (
-              <p className="text-xs text-red-700">{formik?.errors?.author}</p>
-            )}
-          </div>
-
-          <div className="flex flex-col pb-5">
-            <label>Content</label>
-            <textarea
-              id="content"
-              name="content"
-              onChange={formik.handleChange}
-              value={formik.values?.content}
-              placeholder="Drop your ideas'"
-              className="border border-indigo"
-              maxLength={5000}
-            />
-            {formik?.errors && (
-              <p className="text-xs text-red-700">{formik?.errors?.content}</p>
-            )}
-          </div>
-
-          <button type="submit" className="border cursor-pointer">
-            Submit
-          </button>
-        </form>
-      </section>
-    </div>
+    </main>
   );
 }
 

fdaa99e068b13736a758b81baa710c244f5758ad
Novpa <aa.novparadiputra@binus.ac.id>
2026-02-26 10:00:05 +0700

    installing dependencies -tailwind -backendless

---
+++ backendless-blog/src/backendless/backendless.ts
@@ -0,0 +1,8 @@
+import Backendless from "backendless";
+
+const APP_ID = "77EC5ACE-72A3-474F-B05C-87DF54BC44B1";
+const API_KEY = "344771EB-D63A-48AF-A56C-906F92DB3971";
+
+Backendless.initApp(APP_ID, API_KEY);
+
+export default Backendless;

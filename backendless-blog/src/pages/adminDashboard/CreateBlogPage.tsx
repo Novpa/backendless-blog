10c0a7a4ef52931a66432b43ea2d07f08d0cae65
Novpa <aa.novparadiputra@binus.ac.id>
2026-02-27 13:41:01 +0700

    feat/loading indicator

--- backendless-blog/src/pages/adminDashboard/CreateBlogPage.tsx
+++ backendless-blog/src/pages/adminDashboard/CreateBlogPage.tsx
@@ -6,12 +6,15 @@ import { useState } from "react";
 import CofirmationModal from "../../ui/CofirmationModal";
 import Button from "../../ui/Button";
 import FormErrorMessage from "../../ui/FormErrorMessage";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useNavigation } from "react-router-dom";
 import { toast } from "react-toastify";
+import Loader from "../../ui/Loader";
 
 function CreateBlogPage() {
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
+  const navigation = useNavigation();
+  // console.log(navigation.state);
 
   //handle show modal
   const handleShowModal = () => {
@@ -62,88 +65,96 @@ function CreateBlogPage() {
 
   const isError = isWrongInput || !isNotEmpty;
   return (
-    <main className="pt-12 h-dvh ">
-      <div>
-        <div className="flex justify-center pt-12 text-xl md:text-3xl pb-10 ">
-          <h1>Create Log Form</h1>
-        </div>
-
-        <section className="flex justify-center">
-          <div className="w-[85%] sm:w-[75%] md:w-[50%]">
-            <form onSubmit={formik.handleSubmit}>
-              <div className="flex flex-col pb-5">
-                <label className="text-stone-400 text-md md:text-lg font-light">
-                  Blog title
-                </label>
-                <input
-                  id="title"
-                  name="title"
-                  onChange={formik.handleChange}
-                  value={formik.values?.title}
-                  type="text"
-                  placeholder="Input your new Log title"
-                  className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                />
-                {formik?.errors && (
-                  <FormErrorMessage>{formik?.errors?.title}</FormErrorMessage>
-                )}
-              </div>
-
-              <div className="flex flex-col pb-5">
-                <label className="text-stone-400 text-md md:text-lg font-light">
-                  Author
-                </label>
-                <input
-                  id="author"
-                  name="author"
-                  onChange={formik.handleChange}
-                  value={formik.values?.author}
-                  type="text"
-                  placeholder="Input your name as 'Author'"
-                  className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                />
-                {formik?.errors && (
-                  <FormErrorMessage>{formik?.errors?.author}</FormErrorMessage>
-                )}
-              </div>
-
-              <div className="flex flex-col pb-5">
-                <label className="text-stone-400 text-md md:text-lg font-light">
-                  Content
-                </label>
-                <textarea
-                  id="content"
-                  name="content"
-                  onChange={formik.handleChange}
-                  value={formik.values?.content}
-                  placeholder="Drop your ideas"
-                  className="border w-full h-30 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                  maxLength={5000}
-                />
-                {formik?.errors && (
-                  <FormErrorMessage>{formik?.errors?.content}</FormErrorMessage>
-                )}
-                {showModal && (
-                  <CofirmationModal
-                    type="submit"
-                    onCloseModal={handleShowModal}>
-                    Are you sure want to submit this post? You can edit it later
-                  </CofirmationModal>
-                )}
-              </div>
-            </form>
-            {showModal || (
-              <Button
-                disabled={isError}
-                type="button"
-                onClick={handleShowModal}>
-                Submit
-              </Button>
-            )}
+    <>
+      {navigation.state === "loading" && <Loader />}
+      <main className="pt-12 h-dvh ">
+        <div>
+          <div className="flex justify-center pt-12 text-xl md:text-3xl pb-10 ">
+            <h1>Create Log Form</h1>
           </div>
-        </section>
-      </div>
-    </main>
+
+          <section className="flex justify-center">
+            <div className="w-[85%] sm:w-[75%] md:w-[50%]">
+              <form onSubmit={formik.handleSubmit}>
+                <div className="flex flex-col pb-5">
+                  <label className="text-stone-400 text-md md:text-lg font-light">
+                    Blog title
+                  </label>
+                  <input
+                    id="title"
+                    name="title"
+                    onChange={formik.handleChange}
+                    value={formik.values?.title}
+                    type="text"
+                    placeholder="Input your new Log title"
+                    className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                  />
+                  {formik?.errors && (
+                    <FormErrorMessage>{formik?.errors?.title}</FormErrorMessage>
+                  )}
+                </div>
+
+                <div className="flex flex-col pb-5">
+                  <label className="text-stone-400 text-md md:text-lg font-light">
+                    Author
+                  </label>
+                  <input
+                    id="author"
+                    name="author"
+                    onChange={formik.handleChange}
+                    value={formik.values?.author}
+                    type="text"
+                    placeholder="Input your name as 'Author'"
+                    className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                  />
+                  {formik?.errors && (
+                    <FormErrorMessage>
+                      {formik?.errors?.author}
+                    </FormErrorMessage>
+                  )}
+                </div>
+
+                <div className="flex flex-col pb-5">
+                  <label className="text-stone-400 text-md md:text-lg font-light">
+                    Content
+                  </label>
+                  <textarea
+                    id="content"
+                    name="content"
+                    onChange={formik.handleChange}
+                    value={formik.values?.content}
+                    placeholder="Drop your ideas"
+                    className="border w-full h-30 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                    maxLength={5000}
+                  />
+                  {formik?.errors && (
+                    <FormErrorMessage>
+                      {formik?.errors?.content}
+                    </FormErrorMessage>
+                  )}
+                  {showModal && (
+                    <CofirmationModal
+                      type="submit"
+                      onCloseModal={handleShowModal}>
+                      Are you sure want to submit this post? You can edit it
+                      later
+                    </CofirmationModal>
+                  )}
+                </div>
+              </form>
+              {showModal || (
+                <Button
+                  disabled={isError}
+                  type="button"
+                  onClick={handleShowModal}>
+                  Submit
+                </Button>
+              )}
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
 
--- backendless-blog/src/pages/blogFeedsDetails/BlogFeedsDetails.tsx
+++ backendless-blog/src/pages/blogFeedsDetails/BlogFeedsDetails.tsx
@@ -1,6 +1,7 @@
 import {
   useLoaderData,
   useNavigate,
+  useNavigation,
   useParams,
   type ActionFunctionArgs,
 } from "react-router-dom";
@@ -15,6 +16,7 @@ import Button from "../../ui/Button";
 import BackButton from "../../ui/BackButton";
 import FormErrorMessage from "../../ui/FormErrorMessage";
 import { toast } from "react-toastify";
+import Loader from "../../ui/Loader";
 
 function BlogFeedsDetails() {
   const [isEditing, setIsEditing] = useState(false);
@@ -22,6 +24,7 @@ function BlogFeedsDetails() {
   const [showModalDelete, setShowModaldelete] = useState(false);
   const postDetails = useLoaderData();
   const navigate = useNavigate();
+  const navigation = useNavigation();
   const { id } = useParams();
 
   //Handle editing modal
@@ -93,135 +96,143 @@ function BlogFeedsDetails() {
   });
 
   return (
-    <section className="py-30 px-10 sm:px-20 md:px-30">
-      <BackButton>Previous Page</BackButton>
-      <div>
-        <div className="block md:flex md:justify-between items-center">
-          <h1 className="text-3xl lg:text-5xl py-5 md:py-5 w-full md:w-[70%]">
-            {postDetails.title}
-          </h1>
-          <div className="flex gap-5">
-            <Button type="button" onClick={handleIsEditing}>
-              Edit
-            </Button>
-            <Button onClick={handleDeleteModal} type="button" style="secondary">
-              Delete Post
-            </Button>
-          </div>
-        </div>
-        <div className="flex gap-5 text-xs sm:text-sm py-4 text-stone-400 font-light">
-          <p>⦿ {postDetails.author}</p>
-          <p>{formatTimestamp(postDetails.created, postDetails.updated)}</p>
-        </div>
-
-        {showModalDelete && (
-          <CofirmationModal
-            onContinue={handleDeletePost}
-            onCloseModal={handleDeleteModal}>
-            Are you sure want to delete this post?
-          </CofirmationModal>
-        )}
-      </div>
-
-      <section className="py-8 ">
-        <p>{postDetails.content}</p>
-      </section>
-
-      {/* form edit temp */}
-      {isEditing && (
-        <>
-          <section className="absolute top-0 left-0 w-full h-full items-center flex justify-center bg-white/10 backdrop-blur-xs">
-            <div className="w-[85%] bg-black px-20 rounded-xl py-20 sm:w-[75%] md:w-[50%]">
-              <form onSubmit={formik.handleSubmit}>
-                <div className="flex flex-col pb-5">
-                  <label className="text-stone-400 text-md md:text-lg font-light">
-                    New Blog title
-                  </label>
-                  <input
-                    id="title"
-                    name="title"
-                    onChange={formik.handleChange}
-                    value={formik.values?.title}
-                    type="text"
-                    placeholder="Input your new Log title"
-                    className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                  />
-                  {formik?.errors && (
-                    <FormErrorMessage>{formik?.errors?.title}</FormErrorMessage>
-                  )}
-                </div>
-
-                <div className="flex flex-col pb-5">
-                  <label className="text-stone-400 text-md md:text-lg font-light">
-                    New Author
-                  </label>
-                  <input
-                    id="author"
-                    name="author"
-                    onChange={formik.handleChange}
-                    value={formik.values?.author}
-                    type="text"
-                    placeholder="Input your name as 'Author'"
-                    className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                  />
-                  {formik?.errors && (
-                    <FormErrorMessage>
-                      {formik?.errors?.author}
-                    </FormErrorMessage>
-                  )}
-                </div>
-
-                <div className="flex flex-col pb-5">
-                  <label className="text-stone-400 text-md md:text-lg font-light">
-                    Content
-                  </label>
-                  <textarea
-                    id="content"
-                    name="content"
-                    onChange={formik.handleChange}
-                    value={formik.values?.content}
-                    placeholder="Drop your ideas'"
-                    className="border w-full h-30 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
-                    maxLength={5000}
-                  />
-                  {formik?.errors && (
-                    <FormErrorMessage>
-                      {formik?.errors?.content}
-                    </FormErrorMessage>
-                  )}
-                </div>
-
-                <div className="flex gap-5">
-                  {showModalEdit && (
-                    <CofirmationModal
-                      type="submit"
-                      onCloseModal={handleShowModal}>
-                      Are you sure?
-                    </CofirmationModal>
-                  )}
-                </div>
-              </form>
-              {showModalEdit || (
-                <div className="flex gap-5">
-                  <Button
-                    onClick={handleShowModal}
-                    type="button"
-                    style="primary">
-                    Submit
-                  </Button>
-                  <Button
-                    onClick={handleIsEditing}
-                    type="button"
-                    style="secondary">
-                    Cancel
-                  </Button>
-                </div>
-              )}
+    <>
+      {navigation.state === "loading" && <Loader />}
+      <section className="py-30 px-10 sm:px-20 md:px-30">
+        <BackButton>Previous Page</BackButton>
+        <div>
+          <div className="block md:flex md:justify-between items-center">
+            <h1 className="text-3xl lg:text-5xl py-5 md:py-5 w-full md:w-[70%]">
+              {postDetails.title}
+            </h1>
+            <div className="flex gap-5">
+              <Button type="button" onClick={handleIsEditing}>
+                Edit
+              </Button>
+              <Button
+                onClick={handleDeleteModal}
+                type="button"
+                style="secondary">
+                Delete Post
+              </Button>
             </div>
-          </section>
-        </>
-      )}
-    </section>
+          </div>
+          <div className="flex gap-5 text-xs sm:text-sm py-4 text-stone-400 font-light">
+            <p>⦿ {postDetails.author}</p>
+            <p>{formatTimestamp(postDetails.created, postDetails.updated)}</p>
+          </div>
+
+          {showModalDelete && (
+            <CofirmationModal
+              onContinue={handleDeletePost}
+              onCloseModal={handleDeleteModal}>
+              Are you sure want to delete this post?
+            </CofirmationModal>
+          )}
+        </div>
+
+        <section className="py-8 ">
+          <p>{postDetails.content}</p>
+        </section>
+
+        {/* form edit temp */}
+        {isEditing && (
+          <>
+            <section className="absolute top-0 left-0 w-full h-full items-center flex justify-center bg-white/10 backdrop-blur-xs">
+              <div className="w-[85%] bg-black px-20 rounded-xl py-20 sm:w-[75%] md:w-[50%]">
+                <form onSubmit={formik.handleSubmit}>
+                  <div className="flex flex-col pb-5">
+                    <label className="text-stone-400 text-md md:text-lg font-light">
+                      New Blog title
+                    </label>
+                    <input
+                      id="title"
+                      name="title"
+                      onChange={formik.handleChange}
+                      value={formik.values?.title}
+                      type="text"
+                      placeholder="Input your new Log title"
+                      className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                    />
+                    {formik?.errors && (
+                      <FormErrorMessage>
+                        {formik?.errors?.title}
+                      </FormErrorMessage>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col pb-5">
+                    <label className="text-stone-400 text-md md:text-lg font-light">
+                      New Author
+                    </label>
+                    <input
+                      id="author"
+                      name="author"
+                      onChange={formik.handleChange}
+                      value={formik.values?.author}
+                      type="text"
+                      placeholder="Input your name as 'Author'"
+                      className="border w-full h-12 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                    />
+                    {formik?.errors && (
+                      <FormErrorMessage>
+                        {formik?.errors?.author}
+                      </FormErrorMessage>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col pb-5">
+                    <label className="text-stone-400 text-md md:text-lg font-light">
+                      Content
+                    </label>
+                    <textarea
+                      id="content"
+                      name="content"
+                      onChange={formik.handleChange}
+                      value={formik.values?.content}
+                      placeholder="Drop your ideas'"
+                      className="border w-full h-30 px-4 py-2 border-stone-500 rounded-md my-4 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-indigo-500 text-stone-400 font-light"
+                      maxLength={5000}
+                    />
+                    {formik?.errors && (
+                      <FormErrorMessage>
+                        {formik?.errors?.content}
+                      </FormErrorMessage>
+                    )}
+                  </div>
+
+                  <div className="flex gap-5">
+                    {showModalEdit && (
+                      <CofirmationModal
+                        type="submit"
+                        onCloseModal={handleShowModal}>
+                        Are you sure?
+                      </CofirmationModal>
+                    )}
+                  </div>
+                </form>
+                {showModalEdit || (
+                  <div className="flex gap-5">
+                    <Button
+                      onClick={handleShowModal}
+                      type="button"
+                      style="primary">
+                      Submit
+                    </Button>
+                    <Button
+                      onClick={handleIsEditing}
+                      type="button"
+                      style="secondary">
+                      Cancel
+                    </Button>
+                  </div>
+                )}
+              </div>
+            </section>
+          </>
+        )}
+      </section>
+    </>
   );
 }
 

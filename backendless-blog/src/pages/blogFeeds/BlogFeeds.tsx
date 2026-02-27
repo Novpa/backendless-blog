f2e4087f68db5fa4499d4d33e889392dd7d572b4
Novpa <aa.novparadiputra@binus.ac.id>
2026-02-27 10:29:28 +0700

    ref/feeds mobile responsive & styling

--- backendless-blog/src/pages/blogFeeds/BlogFeeds.tsx
+++ backendless-blog/src/pages/blogFeeds/BlogFeeds.tsx
@@ -1,22 +1,32 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import Backendless from "../../backendless/backendless";
+import FeedCard from "./FeedCard";
 
 function BlogFeeds() {
   const posts = useLoaderData();
   console.log("POSTS", posts);
 
   return (
-    <div>
-      <h1>Blog feeds</h1>
-      {posts?.map((post) => (
-        <div key={post.objectId} className="border px-10 py-10">
-          <h2>{post.title}</h2>
-          <p>{post.author}</p>
-          <p>{post.content.slice(0, 100)}...</p>
-          <Link to={`${post.objectId}`}>Read</Link>
-        </div>
-      ))}
-    </div>
+    <main className="pt-30 md:px-10 lg:px-15 xl:px-30">
+      <h1 className="px-10 pb-10 text-3xl md:text-4xl">
+        Blog feeds{" "}
+        <span className="font-extralight text-sm text-indigo-400">_______</span>
+      </h1>
+      <section className="grid grid-cols-2">
+        {posts?.map((post: any, index: number) => (
+          <FeedCard
+            key={post.objectId}
+            index={index}
+            objectId={post.objectId}
+            author={post.author}
+            content={post.content}
+            created={post.created}
+            title={post.title}
+            updated={post.updated}
+          />
+        ))}
+      </section>
+    </main>
   );
 }
 
